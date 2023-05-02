96de6004b40965f44bd479f09acd5d2f02797dd7
juliane <jcasier@protonmail.com>
2023-05-02 10:41:03 +0200

    improving post routes with try and catch and send better messages

--- src/routes/memoBoardRouter.ts
+++ src/routes/memoBoardRouter.ts
@@ -4,23 +4,37 @@ import { db } from '../index';
 const memoBoardRouter = Router();
 
 memoBoardRouter.get('/', async (req, res) => {
-  const [memoBoards] = await db.query(
-    `SELECT board.id, board.board_name
+  try {
+    const [memoBoards] = await db.query(
+      `SELECT board.id, board.board_name
     FROM board;`
-  );
-  res.json(memoBoards);
+    );
+    //---------I think this condition is not working
+    if (memoBoards) res.json(memoBoards);
+    else throw new Error('No boards found!');
+  } catch (err) {
+    console.warn(err);
+    res.status(404).send();
+  }
 });
 
 memoBoardRouter.post('/', async (req, res) => {
   const { boardName } = req.body;
-
-  const resSql = await db.query(
-    `
+  //In databse, board_name is unique
+  try {
+    const [resSql] = await db.query(
+      `
     INSERT INTO board (board_name)
     VALUES (?)
   `,
-    [boardName]
-  );
-  res.status(201).send('Your boardhas been created');
+      [boardName]
+    );
+    res
+      .status(201)
+      .json({ message: 'Your board has been added', resSql: resSql });
+  } catch (err) {
+    console.warn(err);
+    res.status(404).send(err);
+  }
 });
 export default memoBoardRouter;
